e8d6be781decfcedd580402160dffd7f89eb1cbd
Alex <alexrausis@Alexs-MacBook-Air-7.local>
2021-12-19 17:55:23 +0100

    stuff

--- app.py
+++ app.py
@@ -399,6 +399,8 @@ def pump_action2(gpio):
     if request.method == 'GET':
         return render_template('/admin/pump_action.html', gpio_settings=gpio_settings)
     else:
+        print('hello')
+        print(request.form)
         if request.form['submit'] == "Start":
             # turn gpio on
             print('Start: ' + str(gpio))
