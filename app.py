b2eb4c07627b02159cab3a45c6039767a5544030
Alex <alexrausis@Alexs-MacBook-Air-7.local>
2022-01-07 15:34:29 +0100

    ready for new test

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.orm.relationships import RelationshipProperty
 
 RASPI = True
-SCALE = False
+SCALE = True
 #HOST = "127.0.0.1"
 HOST = "192.168.15.203"
 
