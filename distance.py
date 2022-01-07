4a4ab45e73250cf1fe26dadaf1827ce0ac692d19
Alex <alexrausis@Alexs-MacBook-Air-7.local>
2022-01-07 17:28:40 +0100

    added distance sensor

--- distance.py
+++ distance.py
@@ -2,6 +2,16 @@ import RPi.GPIO as GPIO
 import time
  
 
+#GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+ 
+#set GPIO Pins
+GPIO_TRIGGER = 25
+GPIO_ECHO = 26
+ 
+#set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
  
 def distance():
     # set Trigger to HIGH
